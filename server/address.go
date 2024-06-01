4b7d6bfe25ff036f14635bf6135f1bfeb57a80de
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2024-06-01 09:44:48 +0700

    refactor: delete unused files

--- server/address.go
+++
@@ -1,6 +0,0 @@
-package server
-
-type Address struct {
-	IP   string `json:"ip"`
-	Port string `json:"port"`
-}
--- server/node_type.go
+++
@@ -1,9 +0,0 @@
-package server
-
-type NodeType int
-
-const (
-	LEADER NodeType = iota + 1
-	CANDIDATE
-	FOLLOWER
-)
