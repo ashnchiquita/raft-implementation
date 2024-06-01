204b96fa47ab82c581567ae4d513e8ce15a6c9ee
Alisha Listya <73476678+alishalistyaa@users.noreply.github.com>
2024-06-01 23:23:19 +0700

    fix: request vote rpc

--- node/core/request_vote_rpc.go
+++ node/core/request_vote_rpc.go
@@ -5,40 +5,54 @@ import (
 	"tubes.sister/raft/node/data"
 )
 
-func (rn *RaftNode) RequestVote(args *gRPC.RequestVoteArgs, reply *gRPC.RequestVoteReply) error {
+// Updated RequestVote function
+func (rn *RaftNode) RequestVote(args *gRPC.RequestVoteArgs) (*gRPC.RequestVoteReply, error) {
+	// Node
+	currTerm := rn.Persistence.CurrentTerm
+	clusterList := rn.Volatile.ClusterList
+	votedFor := rn.Persistence.VotedFor
+	log := rn.Persistence.Log
+
+	// Reply
+	reply := &gRPC.RequestVoteReply{}
 
 	// Rule 1 : Reply false if term < currentTerm (§5.1)
-	if int(args.Term) < rn.Persistence.CurrentTerm {
-		reply.Term = int32(rn.Persistence.CurrentTerm)
+	if int(args.Term) < currTerm {
+		reply.Term = int32(currTerm)
 		reply.VoteGranted = false
-		return nil
+		return reply, nil
 	}
 
 	// Reset election term if Term > currentTerm
-	if int(args.Term) > rn.Persistence.CurrentTerm {
+	if int(args.Term) > currTerm {
 		rn.Persistence.CurrentTerm = int(args.Term)
 		rn.Volatile.LeaderAddress = data.Address{}
 		rn.Volatile.Type = data.FOLLOWER
 	}
 
 	// Rule 2 : If votedFor is null or candidateId, and candidate’s log is at least as up-to-date as receiver’s log, grant vote (§5.2, §5.4)
-	if (rn.Persistence.VotedFor == data.Address{} ||
-		rn.Persistence.VotedFor == rn.Volatile.ClusterList[args.CandidateId]) &&
-		isUpdated(int(args.LastLogIndex), int(args.LastLogTerm), rn.Persistence.Log) {
-		rn.Persistence.VotedFor = rn.Volatile.ClusterList[args.CandidateId]
+	if (votedFor == data.Address{} ||
+		votedFor == clusterList[args.CandidateId]) &&
+		isUpdated(int(args.LastLogIndex), int(args.LastLogTerm), log) {
+		rn.Persistence.VotedFor = clusterList[args.CandidateId]
 		reply.VoteGranted = true
 	} else {
 		reply.VoteGranted = false
 	}
 
-	reply.Term = int32(rn.Persistence.CurrentTerm)
-	return nil
+	reply.Term = int32(currTerm)
+	return reply, nil
 }
 
+// Updated isUpdated function
 func isUpdated(lastLogIndex int, lastLogTerm int, log []data.LogEntry) bool {
-	if lastLogIndex >= len(log) {
+	if len(log) == 0 {
 		return true
 	}
 
-	return lastLogTerm >= len(log)
+	lastEntry := log[len(log)-1]
+	if lastLogTerm != lastEntry.Term {
+		return lastLogTerm > lastEntry.Term
+	}
+	return lastLogIndex >= len(log)
 }
