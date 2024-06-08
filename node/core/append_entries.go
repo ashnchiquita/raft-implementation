acc1556a404cb2173d3a2cfa88a074d90e9626c9
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2024-06-08 14:49:03 +0700

    fix: test append entries & fixes to rpc

--- node/core/append_entries.go
+++ node/core/append_entries.go
@@ -25,22 +25,23 @@ func (rn *RaftNode) AppendEntries(ctx context.Context, args *gRPC.AppendEntriesA
 
 	// Rule 2: Reply false if log doesn’t contain an
 	// entry at prevLogIndex whose term matches prevLogTerm
-	if len(rn.Persistence.Log) <= int(args.PrevLogIndex) && rn.Persistence.Log[args.PrevLogIndex].Term != int(args.PrevLogTerm) {
+	if len(rn.Persistence.Log) > int(args.PrevLogIndex) && rn.Persistence.Log[args.PrevLogIndex].Term != int(args.PrevLogTerm) {
 		reply.Success = false
 		return reply, nil
 	}
 
 	for i := int(args.PrevLogIndex) + 1; i <= int(args.PrevLogIndex)+len(args.Entries); i++ {
+		argsEntry := args.Entries[i-int(args.PrevLogIndex)-1]
+
 		// Rule 3: If an existing entry conflicts with a new one (same index
 		// but different terms), delete the existing entry and all that
 		// follow it (§5.3)
-		if len(rn.Persistence.Log)-1 >= i && rn.Persistence.Log[i].Term != int(args.Term) {
+		if i < len(rn.Persistence.Log) && rn.Persistence.Log[i].Term != int(argsEntry.Term) {
 			rn.Persistence.Log = rn.Persistence.Log[0:i]
 		}
 
 		// Rule 4: Append any new entries not already in the log
 		if len(rn.Persistence.Log) == i {
-			argsEntry := args.Entries[i-int(args.PrevLogIndex)-1]
 			newEntry := data.LogEntry{Term: int(argsEntry.Term), Command: argsEntry.Command, Value: argsEntry.Value}
 			rn.Persistence.Log = append(rn.Persistence.Log, newEntry)
 			rn.Persistence.Serialize()
@@ -56,12 +57,13 @@ func (rn *RaftNode) AppendEntries(ctx context.Context, args *gRPC.AppendEntriesA
 		// TODO change when key value mechanism is decided
 		splitRes := strings.Split(currentEntry.Value, ",")
 		key := splitRes[0]
-		val := splitRes[1]
 
 		switch command {
 		case "APPEND":
+			val := splitRes[1]
 			rn.Application.Append(key, val)
 		case "SET":
+			val := splitRes[1]
 			rn.Application.Set(key, val)
 		case "DEL":
 			rn.Application.Del(key)
